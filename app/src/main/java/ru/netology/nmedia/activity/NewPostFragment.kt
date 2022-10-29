0270a7312676681193fb60772b964565c4a3a526
Vladimir Chistiakov <stomalux@yandex.ru>
2022-10-29 14:41:56 +1000

    Подготовка к заданию 12_images 29-10-22 0.35 работает . сделана кнопка меню "ок" create_post_menu.xml, изменены fragment_new_post.xml , NewPostFragment.kt

--- app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -1,12 +1,12 @@
 package ru.netology.nmedia.activity
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.FragmentNewPostBinding
 import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.util.StringArg
@@ -27,20 +27,40 @@ class NewPostFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val binding = FragmentNewPostBinding.inflate(
             inflater,
             container,
             false
         )
 
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.create_post_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
+                when (menuItem.itemId) {
+                    R.id.save -> {
+                        viewModel.changeContent(binding.edit.text.toString())
+                        viewModel.save()
+                        AndroidUtils.hideKeyboard(requireView())
+
+                        true
+                    }
+                    else -> false
+                }
+
+        }, viewLifecycleOwner)
+
+
+
         arguments?.textArg
             ?.let(binding.edit::setText)
 
-        binding.ok.setOnClickListener {
-            viewModel.changeContent(binding.edit.text.toString())
-            viewModel.save()
-            AndroidUtils.hideKeyboard(requireView())
-        }
+//        binding.ok.setOnClickListener {
+//перенесли в меню
+//        }
         viewModel.postCreated.observe(viewLifecycleOwner) {
             viewModel.loadPosts()
             findNavController().navigateUp()
